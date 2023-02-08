c999d9cfa669641eeeb568c5fc95756e955511fd
Mangesh Sharma <mangeshsharma4567@gmail.com>
2023-02-08 12:21:40 +0530

    Add profile experience route added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -149,4 +149,57 @@ router.delete('/', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 })
+
+/**
+* @route    api/profile/experience
+* @description Add profile experience
+*/
+
+router.put('/experience',
+    auth,
+    check('title', 'Title is required').notEmpty(),
+    check('company', 'Company is required').notEmpty(),
+    check('from', 'From date is required ').notEmpty(),
+
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const {
+            title,
+            company,
+            location,
+            from,
+            to,
+            current,
+            description
+        } = req.body;
+
+        const newExp = {
+            title,
+            company,
+            location,
+            from,
+            to,
+            current,
+            description
+        }
+
+        try {
+            const profile = await Profile.findOne({ user: req.user.id });
+
+            //Unshift is same as push difference is most recent will be at top
+            profile.experience.unshift(req.body);
+
+            await profile.save();
+
+            res.json(profile);
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send('Server Error');
+        }
+    }
+)
 module.exports = router;
