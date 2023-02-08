c14c58011129cdcf75c6573808b6e03ee9d58da2
Mangesh Sharma <mangeshsharma4567@gmail.com>
2023-02-08 12:34:11 +0530

    Delete experience from profile route added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -202,4 +202,25 @@ router.put('/experience',
         }
     }
 )
+
+/**
+* @route    api/profile/experience/:exp_id
+* @description Delete experience from profile
+*/
+
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+        // Get remove index
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+        console.log(removeIndex)
+        profile.experience.splice(removeIndex, 1);
+        await profile.save();
+        return res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+})
+
 module.exports = router;
