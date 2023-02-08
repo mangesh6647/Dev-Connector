7866122a896c08933df5ecb99836a0a424040290
Mangesh Sharma <mangeshsharma4567@gmail.com>
2023-02-08 19:23:03 +0530

    Comment on a post  route added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -159,4 +159,41 @@ router.put('/unlike/:id', auth, async (req, res) => {
     }
 })
 
+
+/**
+ * @route api/posts/comment/:id
+ * @description Comment on a post 
+ */
+router.post('/comment/:id',
+    auth,
+    check('text', 'Text is required').notEmpty(),
+    async (req, res) => {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        try {
+            const user = await User.findById(req.user.id).select('-password');
+            const post = await Post.findById(req.params.id);
+            const newComment = {
+                text: req.body.text,
+                name: user.name,
+                user: req.user.id
+            };
+
+            post.comments.unshift(newComment);
+            post.save();
+
+            res.json(post.comments);
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send('Server Error');
+        }
+
+
+    }
+);
+
 module.exports = router;
