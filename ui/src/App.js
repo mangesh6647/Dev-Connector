1ff151bcc37c5cb3597fa4beeea184cd58356485
Mangesh Sharma <mangeshsharma4567@gmail.com>
2023-02-10 20:05:02 +0530

    Intial UI setup

---
+++ ui/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import NavBar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Landing />
+      {/* <i className='fa fa-search'></i>
+      <i class="fa-brands fa-google"></i> */}
+    </>
+  );
+}
+
+export default App;
