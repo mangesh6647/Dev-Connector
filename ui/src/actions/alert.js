cc0de9a15bf8df50ddffac0122004c03332affca
Mangesh Sharma <mangeshsharma4567@gmail.com>
2023-02-14 16:46:06 +0530

    Added animation on alert.

--- ui/src/actions/alert.js
+++ ui/src/actions/alert.js
@@ -1,7 +1,7 @@
 import { SET_ALERT, REMOVE_ALERT } from './types';
 import { v4 as uuidv4 } from 'uuid';
 
-export const setAlert = (msg, alertType, timeout = 3000) => dispatch => {
+export const setAlert = (msg, alertType, timeout = 4000) => dispatch => {
     const id = uuidv4();
     dispatch({
         type: SET_ALERT,
