880714a3721ceca41a341158bf34bfb055377e63
Mangesh Sharma <mangeshsharma4567@gmail.com>
2023-02-15 18:30:41 +0530

    Added getUserprofile state

--- ui/src/components/dashboard/Dashboard.js
+++ ui/src/components/dashboard/Dashboard.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
 
-function Dashboard() {
+function Dashboard({ getCurrentProfile, auth, profile }) {
+    useEffect(() => {
+        getCurrentProfile();
+    }, []);
     return (
         <section className="container">
             <div>Dashboard</div>
@@ -8,5 +14,15 @@ function Dashboard() {
     )
 }
 
+Dashboard.propTypes = {
+    getCurrentProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    profile: PropTypes.object.isRequired
+};
 
-export default Dashboard;
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+    profile: state.profile
+});
+
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
