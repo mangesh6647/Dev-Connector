93937288162238a2fb912c7838f8c33d4c55d748
Mangesh Sharma <mangeshsharma4567@gmail.com>
2023-02-19 20:37:52 +0530

    Modified comment form button.

--- ui/src/components/post/CommentForm.js
+++ ui/src/components/post/CommentForm.js
@@ -6,31 +6,17 @@ import { addComment } from '../../actions/post';
 function CommentForm({ postId, addComment }) {
     const [text, setText] = useState('');
     return (
-        <div className='post-form'>
-            <div className='bg-primary p'>
-                <h3>Leave a Comment</h3>
-            </div>
-            <form
-                className='form my-1'
-                onSubmit={e => {
-                    e.preventDefault();
-                    addComment(postId, { text });
-                    setText('');
-                }}
-            >
-                <textarea
-                    name='text'
-                    cols='30'
-                    rows='5'
-                    placeholder='Comment the post'
-                    value={text}
-                    onChange={e => setText(e.target.value)}
-                    required
-                />
-                <input type='submit' className='btn btn-dark my-1' value='Submit' />
+        <div className="message-input">
+            <form onSubmit={e => {
+                e.preventDefault();
+                addComment(postId, { text });
+                setText('');
+            }}>
+                <input type="text" placeholder="Leave a comment..." value={text} onChange={e => setText(e.target.value)} />
+                <button type="submit"><i className="fas fa-paper-plane"></i></button>
             </form>
         </div>
-    );
+    )
 }
 CommentForm.propTypes = {
     addComment: PropTypes.func.isRequired
