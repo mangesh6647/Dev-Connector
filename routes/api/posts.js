470115c61d7bb26aa017940d844edc2edc6c47d3
Mangesh Sharma <mangeshsharma4567@gmail.com>
2023-02-19 14:59:22 +0530

    Removed console log

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -140,7 +140,6 @@ router.put('/like/:id', auth, async (req, res) => {
 router.put('/unlike/:id', auth, async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
-        console.log(post)
         //check if the post is already been liked
         if (post.likes.filter((like) => like.user.toString() === req.user.id).length == 0) {
             return res.status(400).json({ msg: 'Post has not yet been liked' });
