bf6ba62debd4494491f44b5c885465db15bda0f8
Mangesh Sharma <mangeshsharma4567@gmail.com>
2023-02-19 11:33:11 +0530

    Added PostItem component.

---
+++ ui/src/components/posts/PostItem.js
@@ -0,0 +1,61 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Moment from "react-moment";
+import { connect } from 'react-redux';
+import Developer from '../../img/Developer.jpg';
+
+function PostItem({ auth,
+    post: { _id, text, name, user, likes, comments, date } }) {
+    return (
+        <div className="post bg-white p-1 my-1">
+            <div>
+                <a href="profile.html">
+                    <img
+                        className="round-img"
+                        src={Developer}
+                        alt="No Image"
+                    />
+                    <h4>{name}</h4>
+                </a>
+            </div>
+            <div>
+                <p className="my-1">
+                    {text}
+                </p>
+                <p className="post-date">
+                    Posted on <Moment format="DD/MM/YYYY">{date}</Moment>
+                </p>
+                <button type="button" className="btn btn-light">
+                    <i className="fas fa-thumbs-up"></i>{' '}
+                    <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
+                </button>
+                <button type="button" className="btn btn-light">
+                    <i className="fas fa-thumbs-down"></i>
+                </button>
+                <Link to={`/post/${_id}`} className="btn btn-primary">
+                    Discussion {comments.length > 0 && (<span className='comment-count'>{comments.length}</span>)}
+
+                </Link>
+                {!auth.loading && user === auth.user._id && (
+                    <button
+                        type="button"
+                        className="btn btn-danger"
+                    >
+                        <i className="fas fa-times" />
+                    </button>
+                )}
+            </div>
+        </div>
+    )
+}
+
+PostItem.propTypes = {
+    post: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired
+};
+const mapStateToProps = (state) => ({
+    auth: state.auth
+})
+
+export default connect(mapStateToProps, {})(PostItem);
