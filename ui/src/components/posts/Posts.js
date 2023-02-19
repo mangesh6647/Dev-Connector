f597656d2bc6618925994ae2b41edab96a227f45
Mangesh Sharma <mangeshsharma4567@gmail.com>
2023-02-19 11:33:26 +0530

    Added Post Item comp

--- ui/src/components/posts/Posts.js
+++ ui/src/components/posts/Posts.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/post';
+import PostItem from './PostItem';
 import Spinner from '../layout/Spinner';
 
 function Posts({ getPosts, post: { posts, loading } }) {
@@ -9,7 +10,18 @@ function Posts({ getPosts, post: { posts, loading } }) {
         getPosts();
     }, [getPosts]);
     return (
-        <div></div>
+        loading ? <Spinner /> : <section className="container">
+            <h1 className="large text-primary">Posts</h1>
+            <p className="lead">
+                <i className="fas fa-user" /> Welcome to the community
+            </p>
+            {/* <PostForm /> */}
+            <div className="posts">
+                {posts.map((post) => (
+                    <PostItem key={post._id} post={post} />
+                ))}
+            </div>
+        </section>
     )
 }
 
