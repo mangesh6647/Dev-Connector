7338b29c6e6cf957bd73f6619653302f91d4032c
Mangesh Sharma <mangeshsharma4567@gmail.com>
2023-02-22 13:22:36 +0530

    Changes in server.js file for deployment

--- server.js
+++ server.js
@@ -2,16 +2,27 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 
+const path = require('path');
+
 connectDB();
 //Init middleware
 app.use(express.json({ extended: false }));
+//used for local
+//app.get('/', (req, res) => { res.send('API Running ') });
 
-app.get('/', (req, res) => { res.send('API Running ') });
+const __dirname1 = path.resolve();
+
+
 
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+//Used for productioon
+app.use(express.static(path.join(__dirname1, "/ui/build")));
+app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "ui", "build", "index.html"))
+);
 
 const PORT = process.env.PORT || 5000;
 
